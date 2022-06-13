7f5188a8f392250fc004646e07056f7fd32f092b
sajid <sajid04shaikh@gmail.com>
2022-06-13 21:10:46 +0530

    All files added

---
+++ src/MoreInformation.jsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
+import ReactPlayer from 'react-player'
+
+
+
+const MoreInformation = () => {
+    const [ data1 ,setData1] = useState([]);
+   
+    const params = useParams();
+    const [searchParams, setSearchParams] = useSearchParams();
+var id=searchParams.get("id");
+const getData1 = async() => {
+    const response = await fetch(`https://api.spacexdata.com/v4/launches/${id}`);
+    setData1(await response.json());
+    };
+    
+    useEffect(()=> {
+        getData1();
+    console.log(data1);
+    }, [] );
+    
+    var str = JSON.stringify(data1);
+    var JsonArray = JSON.parse(str);
+    let navigate = useNavigate();
+    
+    return(
+        <body>
+<div class="container-fluid bg-1 text-center">
+  <h2 class="margin">{JsonArray.name}</h2>
+  <img src="bird.png" class="img-fluid"  /> 
+  <h3>{JsonArray.date_utc}</h3>
+</div>
+<div class="container-fluid bg-2 text-center">
+  <h3 class="margin">Details</h3>
+  <h4>{JsonArray.details} </h4>
+</div>
+
+<div class='player'>
+    <ReactPlayer url={JSON.stringify(data1.links)} controls={true}/>
+    </div>
+
+    <button className = 'btn btn-primary btn-lg' style={{margin:'20'}} onClick={() =>
+    {
+        navigate('/');
+    }
+    }> Home </button>
+
+     </body>     
+        
+    )
+    
+        
+
+
+    // const renderData =(selectData,index) => {
+    //     return (
+            
+      
+
+    //         <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3"  key = {selectData.id}>
+    //         {
+    //             console.log(visible1)
+    //         }
+    //             <div class="card-flyer">
+    //                 <div class="text-box">
+    //                     <div class="image-box">
+    //                         <img src={selectData.links.patch.small} class = "rounded" alt="small" />
+    //                     </div>
+    //                     <div class="text-container">
+    //                         <h6>{selectData.name}</h6>
+    //                         <div class ="text-limit" >
+    //                         <p>{selectData.details}</p>
+    //                         </div>
+    //                     </div>
+    //                 </div>
+    //             </div>
+        
+    //     </div>  
+
+    //     )
+    // }
+//return();
+
+// <div id="cards_landscape_wrap-2">
+// <div className="container">
+// <div className="row">
+// {data1.slice(0,visible1).map(renderData)}  
+// </div>
+// </div>
+// {visible1 < data1.length &&(
+// <button className='button' onClick={loadMore}><span>Load More</span></button>
+// )}
+// </div>
+
+
+
+
+
+
+}
+export default MoreInformation;
+
+
---
+++ src/fetchFromApi.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
+
+
+
+
+const FetchFromApi = () => {
+    const [ data ,setData] = useState([]);
+    const [visible,setVisible]= useState(12);
+    
+    
+   
+    const getData = async() => {
+        const response = await fetch("https://api.spacexdata.com/v4/launches/past");
+        setData(await response.json());
+       
+     
+    };
+    const loadMore = () =>{
+        setVisible(visible+12)
+    }
+    useEffect(()=> {
+        getData();
+    
+    }, [] );
+
+
+    
+                
+                const renderData =(selectData,index) => {
+                    return (
+                        
+                        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3"  key = {selectData.id}>    
+                            <div class="card-flyer">
+                                <div class="text-box">
+                                    <div class="image-box">
+                                    <Link to={`/moreinformation?id=${selectData.id}`}> <img src={selectData.links.patch.small} class = "rounded" alt="small" /></Link>
+                                    </div>
+                                    <div class="text-container">
+                                        <h6> <Link to={`/moreinformation?id=${selectData.id}`}>{selectData.name}</Link></h6>
+                                        <div class ="text-limit" >
+                                      
+
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                <p>Flight Number:{selectData.flight_number}</p> 
+                            </div>
+                            
+                 
+                    </div>  
+                    
+           
+                    )
+                }
+      return(
+        <div id="cards_landscape_wrap-2">
+        <div class="container">
+          <div class="row">
+            {data.slice(0,visible).map(renderData)}  
+            </div>
+        </div>
+        {visible < data.length &&(
+      <button className='button' onClick={loadMore}><span>Load More</span></button>
+        )}
+      </div>
+      )
+ 
+  
+    
+}
+export default FetchFromApi;
